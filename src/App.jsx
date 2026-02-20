277ae9e2bbd85df42cdb9b7ab8938db81d7a21b7
RAYSSA GABRIELLA RODRIGUES DA SILVA <25162449@edu119.local>
2026-02-20 08:44:04 -0300

    dark login

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export function App(){
+  return (
+    <>
+    <h1> Cadastro</h1>
+    <p> Coloque o seu login para entrar.</p>
+
+    <input type="email" placeholder="Email"/>
+    <input type="password" placeholder="Senha"/>
+
+    <button>Entrar</button>
+    </>
+  )
+}
